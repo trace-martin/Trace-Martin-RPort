faf6b11a655665c075e3522acefc88a35fbe742d
trace-martin <trace.martin7817@gmail.com>
2023-10-17 14:31:04 -0600

    FIXED 404

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import { useLocation } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import "../styles/Navbar.css";
-// import ReorderTwoToneIcon from '@mui/icons-material/ReorderTwoTone';
 import CollectionsIcon from '@mui/icons-material/Collections';
 import HomeIcon from '@mui/icons-material/Home';
 import EmojiPeopleIcon from '@mui/icons-material/EmojiPeople';
@@ -9,54 +8,42 @@ import ContactSupportIcon from '@mui/icons-material/ContactSupport';
 import AccountBoxIcon from '@mui/icons-material/AccountBox';
 
 function Navbar() {
-    const [expandNavbar, setExpandNavBar] = useState(false);
-
-    const location = useLocation();
-
-    useEffect(() => {
-        setExpandNavBar(false)
-    }, [location]);
-
   return (
-      <div className='navbarContainer'>
-            <ul className='navbar'>
-                <li style={{'--i': '#023466', '--j': 'white'}}>
-                    <a href='/'>
-                        <span className='icon'><HomeIcon /></span>
-                        <span className='title'>Home</span>
-                    </a>
-                </li>
-                {/* this is the background color #954495 */}
-                <li style={{'--i': 'white', '--j': '#954495'}}>
-                    <a href='/about'>
-                        <span className='icon'><EmojiPeopleIcon /></span>
-                        <span className='title'>About</span>
-                    </a>
-                </li>
-                <li style={{'--i': '#993e3e', '--j': 'white'}}>
-                    <a href='/projects'>
-                        <span className='icon'><CollectionsIcon /></span>
-                        <span className='title'>Projects</span>
-                    </a>
-                </li>
-                <li style={{'--i': 'white', '--j': '#088c0a'}}>
-                    <a href='/experience'>
-                        <span className='icon'><AccountBoxIcon /></span>
-                        <span className='title'>Experience</span>
-                    </a>
-                </li>
-                <li style={{'--i': '#fffb00', '--j': 'white'}}>
-                    <a href='/contact'>
-                        <span className='icon'><ContactSupportIcon /></span>
-                        <span className='title'>Contact</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    // </div>
+    <div className='navbarContainer'>
+      <ul className='navbar'>
+        <li style={{ '--i': '#023466', '--j': 'white' }}>
+          <Link to="/">
+            <span className='icon'><HomeIcon /></span>
+            <span className='title'>Home</span>
+          </Link>
+        </li>
+        <li style={{ '--i': 'white', '--j': '#954495' }}>
+          <Link to="/about">
+            <span className='icon'><EmojiPeopleIcon /></span>
+            <span className='title'>About</span>
+          </Link>
+        </li>
+        <li style={{ '--i': '#993e3e', '--j': 'white' }}>
+          <Link to="/projects">
+            <span className='icon'><CollectionsIcon /></span>
+            <span className='title'>Projects</span>
+          </Link>
+        </li>
+        <li style={{ '--i': 'white', '--j': '#088c0a' }}>
+          <Link to="/experience">
+            <span className='icon'><AccountBoxIcon /></span>
+            <span className='title'>Experience</span>
+          </Link>
+        </li>
+        <li style={{ '--i': '#fffb00', '--j': 'white' }}>
+          <Link to="/contact">
+            <span className='icon'><ContactSupportIcon /></span>
+            <span className='title'>Contact</span>
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
 
-export default Navbar
-
-
+export default Navbar;
